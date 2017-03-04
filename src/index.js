4c8ce13f1cb49bae1d762e25051004a69ae2a06c
Fernando Daciuk <f.daciuk@gmail.com>
2017-03-04 19:30:55 -0300

    Remove UMD code from coverage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 ;(function (root, moduleName, factory) {
   'use strict'
+  /* istanbul ignore next */
   if (typeof define === 'function' && define.amd) define(moduleName, factory)
   else if (typeof exports === 'object') exports = module.exports = factory()
   else root[moduleName] = factory()
